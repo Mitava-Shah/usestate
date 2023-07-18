443fadb1c43742838e0068eaca4a42b689a53196
mitava-shah <mitvahshah792@gmail.com>
2023-07-18 19:09:14 +0530

    useState Hide/Show event

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const [title,setTitle]=useState(false);
+  return(
+    <>
+      <button className="btn" onClick={()=>setTitle(!title)}>Hide/Show</button>{title}
+      {title? " ":<h2>Welcome to React App</h2>}
+    
+    </>
   );
 }
 
